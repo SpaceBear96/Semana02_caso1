4474d7fab06bfec0e3679f1de6292d7000361d47
SpaceBear96 <cafu.aguilar@tecsup.edu.pe>
2019-08-19 21:21:45 -0500

    commit

---
+++ Semana02_caso3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Semana02_caso3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbAnio = new System.Windows.Forms.ComboBox();
+            this.cbMes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbAnio
+            // 
+            this.cbAnio.FormattingEnabled = true;
+            this.cbAnio.Location = new System.Drawing.Point(21, 12);
+            this.cbAnio.Name = "cbAnio";
+            this.cbAnio.Size = new System.Drawing.Size(121, 21);
+            this.cbAnio.TabIndex = 0;
+            this.cbAnio.SelectedIndexChanged += new System.EventHandler(this.cbAnio_SelectedIndexChanged);
+            // 
+            // cbMes
+            // 
+            this.cbMes.FormattingEnabled = true;
+            this.cbMes.Location = new System.Drawing.Point(215, 12);
+            this.cbMes.Name = "cbMes";
+            this.cbMes.Size = new System.Drawing.Size(121, 21);
+            this.cbMes.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 412);
+            this.Controls.Add(this.cbMes);
+            this.Controls.Add(this.cbAnio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbAnio;
+        private System.Windows.Forms.ComboBox cbMes;
+    }
+}
+
---
+++ Semana02_caso3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Semana02_caso3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["BaseDeDatos"].ConnectionString);
+
+        private void cbAnio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_encontrar_mes", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@anio", cbAnio.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaMes");
+                        cbMes.DataSource = df.Tables["ListaMes"];
+                        cbMes.DisplayMember = "Mes";
+                        cbMes.ValueMember = "Mes";
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+        }
+
+        private void ListaAnios() {
+            using (SqlCommand cmd = new SqlCommand("usp_encontrar_anio", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "ListaAnios");
+                        cbAnio.DataSource = df.Tables["ListaAnios"];
+                        cbAnio.DisplayMember = "Anios";
+                        cbAnio.ValueMember = "Anios";
+                    }
+                }
+            }
+        }
+
+
+    }
+}
