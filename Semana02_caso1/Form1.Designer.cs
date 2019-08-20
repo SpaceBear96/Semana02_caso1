d060adca5ed9d0ecf91d5934b1ba334330a32e33
SpaceBear96 <cafu.aguilar@tecsup.edu.pe>
2019-08-19 20:37:35 -0500

    commit

--- Semana02_caso1/Form1.Designer.cs
+++ Semana02_caso1/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFiltro = new System.Windows.Forms.TextBox();
+            this.dgDatos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.var = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE O APELLIDO";
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.Location = new System.Drawing.Point(139, 22);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(155, 20);
+            this.lblFiltro.TabIndex = 1;
+            this.lblFiltro.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dgDatos
+            // 
+            this.dgDatos.AllowUserToAddRows = false;
+            this.dgDatos.AllowUserToDeleteRows = false;
+            this.dgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDatos.Location = new System.Drawing.Point(16, 53);
+            this.dgDatos.Name = "dgDatos";
+            this.dgDatos.ReadOnly = true;
+            this.dgDatos.Size = new System.Drawing.Size(772, 237);
+            this.dgDatos.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // var
+            // 
+            this.var.AutoSize = true;
+            this.var.Location = new System.Drawing.Point(451, 25);
+            this.var.Name = "var";
+            this.var.Size = new System.Drawing.Size(35, 13);
+            this.var.TabIndex = 4;
+            this.var.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 308);
+            this.Controls.Add(this.var);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgDatos);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblFiltro;
+        private System.Windows.Forms.DataGridView dgDatos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label var;
     }
 }
 
--- Semana02_caso1/Form1.cs
+++ Semana02_caso1/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Configuration;
+using System.Data.SqlClient;
 namespace Semana02_caso1
 {
     public partial class Form1 : Form
@@ -15,6 +16,49 @@ namespace Semana02_caso1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["BaseDeDatos"].ConnectionString);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_nombre_apellido", cn))
+            {   
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "listanomape");
+                        dgDatos.DataSource = df.Tables["listanomape"];
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("usp_filtro_nomape", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@nomape",lblFiltro.Text);
+                    var.Text = lblFiltro.Text;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "listanomape");
+                        dgDatos.DataSource = df.Tables["listanomape"];
+                    }
+                }
+            }
         }
     }
 }
